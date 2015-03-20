cd368cdd59bb309c282dcd98788025f471c19df3
lexfaraday <lexfaraday@outlook.com>
2015-03-20 17:44:05 +0100

    Add post method getting event list

--- EventsApp/public/modules/events/controllers/events.client.controller.js
+++ EventsApp/public/modules/events/controllers/events.client.controller.js
@@ -1,13 +1,36 @@
 'use strict';
 
 
-angular.module('events').controller('EventsController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('events').controller('EventsController', ['$scope', 'Authentication', '$http',
+	function($scope, Authentication, $http) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
 		jQuery(document).ready(function() {
 			initialize();
+
+		    if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(function(position){
+		        	var req = {
+						method: 'POST',
+					 	url: 'http://localhost:8080/api/listevents',
+					 	headers: {
+					   	'Content-Type': undefined
+					 	},
+					 	data: { latitude: position.coords.latitude, longitude: position.coords.longitude },
+					}
+		        	$http(req).
+  						success(function(data, status, headers, config) {
+    						console.log(data);
+  					}).
+  						error(function(data, status, headers, config) {
+    						console.log(data);
+  					});
+		        });
+		    } else { 
+		        console.error("Geolocation is not supported by this browser.");
+		    }
+
 		});
 	}
 ]);
