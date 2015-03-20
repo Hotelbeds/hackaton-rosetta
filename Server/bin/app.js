7661810f33101105a43930a8176f824a71290c49
DDani <danicastro@gmail.com>
2015-03-20 17:51:48 +0100

    -Enabled CORS support

--- Server/bin/app.js
+++ Server/bin/app.js
@@ -26,7 +26,29 @@ exports.startServer = function(port, callback) {
 		port = config.expressPort;
 	}
 	var app = express();
-	// GET requests
+
+    // Enable cross domain origin
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
+
+    // GET requests
 	app.get('/api/:service', serve);
 	// load services
 	services = readServices();
